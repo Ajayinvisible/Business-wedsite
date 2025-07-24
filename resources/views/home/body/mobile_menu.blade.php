d246510433e1758be9644e50af26f83ed232851e
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-25 00:38:15 +0545

    feat: fix menu in desktop and mobile view

--- resources/views/home/body/mobile_menu.blade.php
+++ resources/views/home/body/mobile_menu.blade.php
@@ -12,90 +12,24 @@
          </div>
          <div class="lonyo-mobile-menu">
              <ul>
-                 <li class="menu-item-has-children">
-                     <a href="#">Demo</a>
-                     <ul class="sub-menu">
-                         <li>
-                             <a href="index.html">
-                                 Home 01
-                             </a>
-                         </li>
-                         <li>
-                             <a href="index-02.html">
-                                 Home 02
-                             </a>
-                         </li>
-                         <li>
-                             <a href="index-03.html">
-                                 Home 03
-                             </a>
-                         </li>
-                     </ul>
+                 <li>
+                     <a href="#">Home</a>
                  </li>
                  <li class="menu-item-has-children">
-                     <a href="#">Pages</a>
+                     <a href="#">About us</a>
                      <ul class="sub-menu">
-                         <li><a href="about-us.html">About Us</a></li>
-                         <li><a href="pricing.html">Pricing</a></li>
-                         <li class="menu-item-has-children">
-                             <a class="no-border" href="#">Integratios</a>
-                             <ul class="sub-menu">
-                                 <li><a href="integration.html">Integratios</a></li>
-                                 <li><a href="single-integration.html">Integratios Details</a></li>
-                             </ul>
-                         </li>
-                         <li class="menu-item-has-children">
-                             <a class="no-border" href="#">Team</a>
-                             <ul class="sub-menu">
-                                 <li><a href="team.html">team</a></li>
-                                 <li><a href="single-team.html">team details</a></li>
-                             </ul>
-                         </li>
-                         <li class="menu-item-has-children">
-                             <a class="no-border" href="#">Service</a>
-                             <ul class="sub-menu">
-                                 <li><a href="service.html">Service</a></li>
-                                 <li><a href="single-service.html">Service Details</a></li>
-                             </ul>
-                         </li>
-                         <li class="menu-item-has-children">
-                             <a class="no-border" href="#">Career</a>
-                             <ul class="sub-menu">
-                                 <li><a href="career.html">Career</a></li>
-                                 <li><a href="single-career.html">Career details</a></li>
-                             </ul>
-                         </li>
-                         <li class="menu-item-has-children">
-                             <a class="no-border" href="#">Utility</a>
-                             <ul class="sub-menu">
-                                 <li><a href="faq.html">faq</a></li>
-                                 <li><a href="errors-404.html">errors 404</a></li>
-                                 <li><a href="cooming-soon.html">Cooming Soon</a></li>
-                             </ul>
-                         </li>
-                         <li class="menu-item-has-children">
-                             <a class="no-border" href="#">Accounts</a>
-                             <ul class="sub-menu">
-                                 <li><a href="sign-up.html">Sign Up</a></li>
-                                 <li><a href="sign-in.html">Sign In</a></li>
-                                 <li><a href="reset-password.html">Reset Password</a></li>
-                             </ul>
-                         </li>
+                         <li><a href="about-us.html">Company Profile</a></li>
+                         <li><a href="pricing.html">Team</a></li>
                      </ul>
                  </li>
-                 <li class="menu-item-has-children">
+                 <li>
+                     <a href="#">Our Service</a>
+                 </li>
+                 <li>
                      <a href="#">Portfolio</a>
-                     <ul class="sub-menu">
-                         <li><a href="portfolio.html">Portfolio</a></li>
-                         <li><a href="single-portfolio.html">Portfolio Details</a></li>
-                     </ul>
                  </li>
-                 <li class="menu-item-has-children">
+                 <li>
                      <a href="#">Blog</a>
-                     <ul class="sub-menu">
-                         <li><a href="blog.html">Blog</a></li>
-                         <li><a href="single-blog.html">Blog Details</a></li>
-                     </ul>
                  </li>
                  <li>
                      <a href="contact-us.html">Contact</a>
