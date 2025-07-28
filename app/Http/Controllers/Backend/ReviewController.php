062bd6167906367af748cc6fbcd3ce8a02380267
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-28 23:47:18 +0545

    feat: fetch data from database review

--- app/Http/Controllers/Backend/ReviewController.php
+++ app/Http/Controllers/Backend/ReviewController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function AllReview()
+    {
+        // Logic to retrieve all reviews
+        $reviews = Review::latest()->get();
+        // Return the view with the reviews data
+        return view('admin.backend.review.all_review',[
+            'reviews' => $reviews
+        ]);
+    }
 }
