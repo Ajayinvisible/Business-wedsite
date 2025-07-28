ac313d65f023f9fd683ef2f21f32cd7bd729e478
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-29 00:08:40 +0545

    feat: review datatable setup

--- resources/views/admin/backend/review/all_review.blade.php
+++ resources/views/admin/backend/review/all_review.blade.php
@@ -8,10 +8,54 @@
 
             <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
                 <div class="flex-grow-1">
-                    <h4 class="fs-18 fw-semibold m-0">Dashboard</h4>
+                    <h4 class="fs-18 fw-semibold m-0">All Review</h4>
                 </div>
             </div>
 
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+
+                        <div class="card-header">
+                            <h5 class="card-title mb-0">All Review</h5>
+                        </div><!-- end card header -->
+
+                        <div class="card-body">
+                            <table id="datatable" class="table table-bordered dt-responsive table-responsive nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>S.n</th>
+                                        <th>Name</th>
+                                        <th>Position</th>
+                                        <th>Image</th>
+                                        <th>Message</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($reviews as $key => $review)
+                                        <tr>
+                                            <td>{{ ++$key }}</td>
+                                            <td>{{ $review->name }}</td>
+                                            <td>{{ $review->position }}</td>
+                                            <td>
+                                                <img src="{{ asset($review->image) }}" alt="{{ $review->name }}"
+                                                    class="rounded-circle" style="width: 50px; height: 50px;">
+                                            </td>
+                                            <td>{!! $review->message !!}</td>
+                                            <td>
+                                                <a href="" class="btn btn-success rounded-pill">Success</a>
+                                                <a href="" type="button" class="btn btn-danger rounded-pill">Danger</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
         </div> <!-- container-fluid -->
     </div> <!-- content -->
