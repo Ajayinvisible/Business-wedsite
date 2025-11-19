83f73526c8c95bdab1f2ae6c4012bd2670fc421a
Ajay Rai <rai.invisibleweb@gmail.com>
2025-11-20 00:12:55 +0545

    feat: team member fetch in team page

--- resources/views/home/team/team_page.blade.php
+++ resources/views/home/team/team_page.blade.php
@@ -30,20 +30,26 @@
              <div class="lonyo-section-title max-w616">
                  <h2>Meet our brilliant team members</h2>
              </div>
+             @php
+                 $teams = App\Models\Team::latest()->get();
+             @endphp
              <div class="row">
-                 <div class="col-lg-3 col-md-6">
-                     <div class="lonyo-team-wrap" data-aos="fade-up" data-aos-duration="500">
-                         <div class="lonyo-team-thumb">
-                             <a href="single-team.html"><img src="{{ asset('frontend/assets/images/about-us/t1.png') }}" alt=""></a>
-                         </div>
-                         <div class="lonyo-team-content2">
-                             <a href="single-team.html">
-                                 <h6>Michael Chen</h6>
-                             </a>
-                             <p>Chief Executive Officer</p>
+                 @foreach ($teams as $team)
+                     <div class="col-lg-3 col-md-6">
+                         <div class="lonyo-team-wrap" data-aos="fade-up" data-aos-duration="500">
+                             <div class="lonyo-team-thumb">
+                                 <a href="single-team.html"><img src="{{ asset($team->image) }}"
+                                         alt="{{ $team->name }}"></a>
+                             </div>
+                             <div class="lonyo-team-content2">
+                                 <a href="single-team.html">
+                                     <h6>{{ $team->name }}</h6>
+                                 </a>
+                                 <p>{{ $team->position }}</p>
+                             </div>
                          </div>
                      </div>
-                 </div>
+                 @endforeach
              </div>
              <div class="mt-50 team-btn" data-aos="fade-up" data-aos-duration="700">
                  <a href="contact-us.html" class="lonyo-default-btn team-btn2">Would you joint of our group?</a>
